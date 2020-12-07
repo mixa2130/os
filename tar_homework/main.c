7379b2e243d2bbc3a9b5909944bfb0f1e1d0979e
Michael S2pac <mixa21.11@mail.ru>
2020-12-08 02:22:58 +0300

    Added tar destructor and checking for non-allocation of memory
    some code refactoring

--- tar_homework/main.c
+++ tar_homework/main.c
@@ -3,122 +3,138 @@
 #include <string.h>
 #include <sys/stat.h>
 
+#define ERROR_ALLOCATE_MEMORY -2
 #define ERROR_OPEN_FILE -3
 #define ERROR_GET_FILE_INFO -4
 
-#define GREETING_BYTES 12//Watermark \
-                            //          \
-                            //
-// обработчик malloc: if null - освободить
-//if (numbers == NULL) {  // если память не дали
-//        fprintf(stderr, "Error allocate memory.\n");    // сообщаем об ошибке
-//        return 2;           // выходим с кодом ошибки
-//    }
+#define GREETING_BYTES 12
+
 struct file_info {
-    size_t name_length;
-    int file_size;// в байтах
-    //    int file_offset;// смещение относительно начала файла
-    // положение файл в массиве файлов будет определяться индексом данной структуры в tat.file_infos
+  size_t name_length;
+  int file_size; // в байтах
+  //    int file_offset;// смещение относительно начала файла
+
+  // положение файл в массиве файлов будет определяться индексом данной
+  // структуры в tar.file_infos
 };
 
 struct tar {
-    const char *header;
-    int file_count;
-    struct file_info *file_infos;
-    char **filenames;
-    char **file_contents;
+  const char *header;
+  int file_count;
+  struct file_info *file_infos;
+  char **filenames;
+  char **file_contents;
 };
 
 int get_file_binary_size(const char *filename) {
-    struct stat sb;
+  struct stat sb;
 
-    int ret = stat(filename, &sb);
-    if (ret) {
-        printf("Error opening file");
-        exit(ERROR_GET_FILE_INFO);
-    }
+  int ret = stat(filename, &sb);
+  if (ret) {
+    fprintf(stderr, "Error open file info");
+    exit(ERROR_GET_FILE_INFO);
+  }
 
-    return sb.st_size;
+  return sb.st_size;
 }
 
-char *with_open_file(const char *filename,
-                     const char *mode,
+char *with_open_file(const char *filename, const char *mode,
                      struct file_info *file_description) {
-    FILE *file = fopen(filename, mode);
+  FILE *file = fopen(filename, mode);
 
-    if (file == NULL) {
-        printf("Error opening file");
-        exit(ERROR_OPEN_FILE);
-    }
+  if (file == NULL) {
+    fprintf(stderr, "Error opening file");
+    exit(ERROR_OPEN_FILE);
+  }
 
-    int size = get_file_binary_size(filename);
-    file_description->name_length = strlen(filename);
-    file_description->file_size = size;
+  int size = get_file_binary_size(filename);
+  file_description->name_length = strlen(filename);
+  file_description->file_size = size;
 
-    char *content = (char *) malloc(size);
-    fread(content, sizeof(char), size, file);
+  char *content = (char *)malloc(size * sizeof(char));
+  if (content == NULL) {
+    fprintf(stderr, "Error allocate memory!\n");
+    exit(ERROR_ALLOCATE_MEMORY);
+  }
 
-    fclose(file);
-    return content;
+  fread(content, sizeof(char), size, file);
+
+  fclose(file);
+  return content;
 }
 
 void collect_tar_(int count, char *argv[], struct tar *my_tar) {
-    int file_count = count - 4;
+  int file_count = count - 4;
 
-    my_tar->header = "Watermark";
-    my_tar->file_count = file_count;
+  my_tar->header = "Watermark";
+  my_tar->file_count = file_count;
 
-    my_tar->filenames = (char **) malloc(file_count);
-    my_tar->file_contents = (char **) malloc(file_count);
-    my_tar->file_infos = (struct file_info *) malloc(file_count * sizeof(struct file_info));
+  my_tar->filenames = (char **)malloc(file_count * sizeof(char *));
+  my_tar->file_contents = (char **)malloc(file_count * sizeof(char *));
+  my_tar->file_infos =
+      (struct file_info *)malloc(file_count * sizeof(struct file_info));
 
-    for (int i = 3, j = 0; i < count - 1; i++, j++) {
-        my_tar->filenames[j] = (char *) malloc(strlen(argv[i]));
-        my_tar->filenames[j] = argv[i];
+  if (my_tar->filenames == NULL || my_tar->file_contents == NULL ||
+      my_tar->file_infos == NULL) {
+    fprintf(stderr, "Error allocate memory!\n");
+    exit(ERROR_ALLOCATE_MEMORY);
+  }
 
-        my_tar->file_contents[j] = with_open_file(argv[i], "rb", &my_tar->file_infos[j]);
+  for (int i = 3, j = 0; i < count - 1; i++, j++) {
+    my_tar->filenames[j] = (char *)malloc(strlen(argv[i]) * sizeof(char));
+    if (my_tar->filenames[j] == NULL) {
+      fprintf(stderr, "Error allocate memory!\n");
+      exit(ERROR_ALLOCATE_MEMORY);
     }
+
+    my_tar->filenames[j] = argv[i];
+    my_tar->file_contents[j] =
+        with_open_file(argv[i], "rb", &my_tar->file_infos[j]);
+  }
 }
 
-int write_tar(int argc, char *argv[]){
-    struct tar my_tar;
-    collect_tar_(argc, argv, &my_tar);
+void tar_destructor(struct tar *my_tar) {
+  for (int i = 0; i < my_tar->file_count; i++) {
+    my_tar->file_contents[i] = 0;
+    free(my_tar->file_contents[i]);
 
+    my_tar->filenames[i] = 0;
+    free(my_tar->filenames[i]);
+  }
+  free(my_tar->filenames);
+  free(my_tar->file_contents);
+  free(my_tar->file_infos);
+}
 
+int write_tar(int argc, char *argv[]) {
+  struct tar my_tar;
+  collect_tar_(argc, argv, &my_tar);
+
+  FILE *file = fopen(argv[argc - 1], "wb");
+
+  if (file == NULL) {
+    printf("Error opening file");
+    exit(ERROR_OPEN_FILE);
+  }
+
+  // write tar to binary file
+
+  fclose(file);
+  tar_destructor(&my_tar);
+
+  return 0;
 }
 
 int main(int argc, char *argv[]) {
-    char *file;
-    if (argc < 5) {
-        printf("Not enough arguments\n");
-        return -1;
-    }
+  char *file;
+  if (argc < 5) {
+    printf("Not enough arguments\n");
+    return -1;
+  }
 
-    if (!strcmp(argv[2], "-c")) {
-        int smth = write_tar(argc, argv);
-        //        printf("%d", smth);
-    }
-    return 0;
+  if (!strcmp(argv[2], "-c")) {
+    int smth = write_tar(argc, argv);
+  }
+
+  return 0;
 }
-
-
-//void main() {
-//    FILE *iofile = NULL;
-//    unsigned counter;
-//    int i, num;
-//
-//    iofile = fopen("numbers.bin", "rb");
-//    if (iofile == NULL) {
-//        printf("Error opening file");
-//        exit(ERROR_OPEN_FILE);
-//    }
-//
-//    fread(&counter, sizeof(int), 1, iofile);
-//    printf("%u\n",counter);
-//    for (i = 0; i < counter; i++) {
-//        fread(&num, sizeof(int), 1, iofile);
-//        printf("%d\n", num);
-//    }
-//
-//    fclose(iofile);
-//}

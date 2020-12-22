2397e9bd0862ce4586f6bce2a223e2b0fc4fff5b
Michael S2pac <mixa21.11@mail.ru>
2020-12-22 06:47:15 +0300

    Some code refactoting at lab5/

--- lab5/main.c
+++ lab5/main.c
@@ -3,14 +3,14 @@
 #include <unistd.h>
 
 #define READER_THREADS_NUM 10
-#define ARRAY_SIZE 10
+#define ARRAY_SIZE 20
 
 
 pthread_mutex_t threads_mutex = PTHREAD_MUTEX_INITIALIZER;
 int counter = 0;
 
 /* In this array will store ASCII symbols
- * relevant to counter number + 65*/
+ * relevant to counter number + 64*/
 char common_thread_arr[ARRAY_SIZE];
 
 
@@ -18,14 +18,15 @@ void *writer(void *args) {
     while (counter < ARRAY_SIZE) {
 
         pthread_mutex_lock(&threads_mutex);
+
             common_thread_arr[counter++] = counter + 65;
             printf("counter:%d\n", counter); // new step
+
         pthread_mutex_unlock(&threads_mutex);
 
         sleep(1);
     }
 
-//    printf("Write thread: %lu has ended\n", pthread_self());
     pthread_exit(0);
 }
 
@@ -40,34 +41,36 @@ void *reader(void *args) {
 
         pthread_mutex_unlock(&threads_mutex);
 
-        if (counter == ARRAY_SIZE) {
-//            pthread_mutex_lock(&threads_mutex);
-//            printf("Thread: %lu has ended\n", pthread_self());
-//            pthread_mutex_unlock(&threads_mutex);
+        if (counter == ARRAY_SIZE)
             pthread_exit(0);
-        }
+
 
         sleep(1);
-
-
     }
 }
 
 
-int main() {
+int main(void) {
     pthread_t reader_threads[READER_THREADS_NUM];
     pthread_t writer_thread;
+
+
 
     for (int i = 0; i < READER_THREADS_NUM; i++)
         pthread_create(&reader_threads[i], NULL, reader, NULL);
     pthread_create(&writer_thread, NULL, writer, NULL);
 
-    for (int i = 0; i< READER_THREADS_NUM; i++) {
+
+
+    for (int i = 0; i< READER_THREADS_NUM; i++)
         pthread_join(reader_threads[i], NULL);
-    }
     pthread_join(writer_thread, NULL);
 
-    printf("In a galaxy far far away...\nTHE END");
+    printf("\nIn a galaxy far far away...\nTHE END\n");
+
+
+
     pthread_mutex_destroy(&threads_mutex);
+
     return 0;
 }
